be8e2383e2cc7b5ff70fcaf789c78441bf7a60b3
quanganh2001 <cocoi5659@gmail.com>
2025-10-20 22:17:51 +0700

    Validate form với thư viện Zod, tạo giao diện SignIn

--- frontend/src/components/auth/signup-form.tsx
+++ frontend/src/components/auth/signup-form.tsx
@@ -2,14 +2,39 @@ import { cn } from '@/lib/utils';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent } from '@/components/ui/card';
 import { Input } from '@/components/ui/input';
-import { Label } from './ui/label';
+import { z } from 'zod';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Label } from '../ui/label';
+
+const signUpSchema = z.object({
+  firstname: z.string().min(1, 'Tên bắt buộc phải có'),
+  lastname: z.string().min(1, 'Họ bắt buộc phải có'),
+  username: z.string().min(3, 'Tên đăng nhập phải có ít nhất 3 ký tự'),
+  email: z.email('Email không hợp lệ'),
+  password: z.string().min(6, 'Mật khẩu phải có ít nhất 6 ký tự'),
+});
+
+type SignUpFormValues = z.infer<typeof signUpSchema>;
 
 export function SignupForm({ className, ...props }: React.ComponentProps<'div'>) {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<SignUpFormValues>({
+    resolver: zodResolver(signUpSchema),
+  });
+
+  const onSubmit = async (data: SignUpFormValues) => {
+    // gọi backend để signup
+  };
+
   return (
     <div className={cn('flex flex-col gap-6', className)} {...props}>
       <Card className="overflow-hidden p-0 border-border">
         <CardContent className="grid p-0 md:grid-cols-2">
-          <form className="p-6 md:p-8">
+          <form className="p-6 md:p-8" onSubmit={handleSubmit(onSubmit)}>
             <div className="flex flex-col gap-6">
               {/* header - logo */}
               <div className="flex flex-col items-center text-center gap-2">
@@ -27,15 +52,17 @@ export function SignupForm({ className, ...props }: React.ComponentProps<'div'>)
                   <Label htmlFor="lastname" className="block text-sm">
                     Họ
                   </Label>
-                  <Input type="text" id="lastname" />
-                  {/* todo: error message */}
+                  <Input type="text" id="lastname" {...register('lastname')} />
+
+                  {errors.lastname && <p className="text-destructive text-sm">{errors.lastname.message}</p>}
                 </div>
                 <div className="space-y-2">
                   <Label htmlFor="firstname" className="block text-sm">
                     Tên
                   </Label>
-                  <Input type="text" id="firstname" />
-                  {/* todo: error message */}
+                  <Input type="text" id="firstname" {...register('firstname')} />
+
+                  {errors.firstname && <p className="text-destructive text-sm">{errors.firstname.message}</p>}
                 </div>
               </div>
 
@@ -44,8 +71,9 @@ export function SignupForm({ className, ...props }: React.ComponentProps<'div'>)
                 <Label htmlFor="username" className="block text-sm">
                   Tên đăng nhập
                 </Label>
-                <Input type="text" id="username" placeholder="moji" />
-                {/* todo: error message */}
+                <Input type="text" id="username" placeholder="moji" {...register('username')} />
+
+                {errors.username && <p className="text-destructive text-sm">{errors.username.message}</p>}
               </div>
 
               {/* email */}
@@ -53,8 +81,9 @@ export function SignupForm({ className, ...props }: React.ComponentProps<'div'>)
                 <Label htmlFor="email" className="block text-sm">
                   Email
                 </Label>
-                <Input type="email" id="email" placeholder="m@gmail.com" />
-                {/* todo: error message */}
+                <Input type="email" id="email" placeholder="m@gmail.com" {...register('email')} />
+
+                {errors.email && <p className="text-destructive text-sm">{errors.email.message}</p>}
               </div>
 
               {/* password */}
@@ -62,12 +91,13 @@ export function SignupForm({ className, ...props }: React.ComponentProps<'div'>)
                 <Label htmlFor="password" className="block text-sm">
                   Mật khẩu
                 </Label>
-                <Input type="password" id="password" />
-                {/* todo: error message */}
+                <Input type="password" id="password" {...register('password')} />
+
+                {errors.password && <p className="text-destructive text-sm">{errors.password.message}</p>}
               </div>
 
               {/* nút đăng ký */}
-              <Button type="submit" className="w-full">
+              <Button type="submit" className="w-full" disabled={isSubmitting}>
                 Tạo tài khoản
               </Button>
 
